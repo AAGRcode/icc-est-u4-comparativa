d5143ac924476c2316e8bdaaafd177ff13b31a80
AAGRcode <axel.gonzalez.280907@gmail.com>
2026-06-01 08:58:31 -0500

    tercer commit

--- src/App.java
+++ src/App.java
@@ -1,9 +1,8 @@
+import controllers.SortPersonaMethods;
 import models.Persona;
+import models.Resultado;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
     public static Persona[] generarPersonas(int cantidad){
         Persona[] personas = new Persona[cantidad];
         for(int i=0; i<cantidad; i++){
@@ -13,4 +12,29 @@ public class App {
         }    
         return personas;
     }
+    public static void main(String[] args) throws Exception {
+        SortPersonaMethods sorter = new SortPersonaMethods();
+        int[] cantidades = {10000, 50000, 100000};
+
+        for (int c : cantidades) {
+            Persona[] base = generarPersonas(c);
+
+            Persona[] paraInsercion = base.clone();
+            Persona[] paraQuick = base.clone();
+
+            Resultado resInsercion = BenchMarking.medirTiempo(
+                () -> { sorter.insertionSort(paraInsercion); return null; },
+                "Inserción", "Desordenado", c
+            );
+
+            Resultado resQuick = BenchMarking.medirTiempo(
+                () -> { sorter.quickSort(paraQuick, 0, paraQuick.length - 1); return null; },
+                "QuickSort", "Desordenado", c
+            );
+
+            System.out.println("Desordenado | Inserción | " + resInsercion.getSample() + " | " + resInsercion.getTiempoMilis() + " ms");
+            System.out.println("Desordenado | QuickSort | " + resQuick.getSample() + " | " + resQuick.getTiempoMilis() + " ms");
+    }
+    
+}
 }
